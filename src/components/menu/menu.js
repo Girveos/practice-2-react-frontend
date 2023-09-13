adccf1d9e3f91bee1787cfca7782afa992edcd70
Girveos <jero713@hotmail.com>
2023-09-13 12:07:43 -0500

    slideImplementation

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,7 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import './menu.scss'; // Importa los estilos de SCSS
-import avatar from '../../assets/images/avatar.png'
-import logo from '../../assets/images/Logos_UAM-08.png'
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./menu.scss"; // Importa los estilos de SCSS
+import avatar from "../../assets/images/avatar.png";
+import logo from "../../assets/images/Logos_UAM-08.png";
+import maluma from "../../assets/images/maluma.gif";
+import maluma1 from "../../assets/images/Maluma1.gif";
+import eladio from "../../assets/images/eladio.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
@@ -14,14 +20,14 @@ function MenuReact() {
 
   useEffect(() => {
     const handleMenuToggle = () => {
-      const menuList = document.getElementById('menu-list');
-      menuList.classList.toggle('show-menu');
+      const menuList = document.getElementById("menu-list");
+      menuList.classList.toggle("show-menu");
     };
 
     const handleResize = () => {
       if (window.innerWidth > 530) {
-        const menuList = document.getElementById('menu-list');
-        menuList.classList.remove('show-menu');
+        const menuList = document.getElementById("menu-list");
+        menuList.classList.remove("show-menu");
       }
     };
 
@@ -41,61 +47,107 @@ function MenuReact() {
     };
 
     // Agrega eventos de escucha para el menú, el redimensionamiento de la ventana y el desplazamiento
-    const menuToggle = document.getElementById('menu-toggle');
-    menuToggle.addEventListener('click', handleMenuToggle);
-    window.addEventListener('resize', handleResize);
-    window.addEventListener('scroll', handleScroll);
+    const menuToggle = document.getElementById("menu-toggle");
+    menuToggle.addEventListener("click", handleMenuToggle);
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("scroll", handleScroll);
 
     // Asegúrate de eliminar los eventos de escucha cuando el componente se desmonte
     return () => {
-      menuToggle.removeEventListener('click', handleMenuToggle);
-      window.removeEventListener('resize', handleResize);
-      window.removeEventListener('scroll', handleScroll);
+      menuToggle.removeEventListener("click", handleMenuToggle);
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     <div className="container">
       {/* Sección de iconos que se mostrará al desplazarse hacia abajo */}
       {showIcons && (
         <div className="icon-section">
-          <a href="#" class="iconlist">
-                <FontAwesomeIcon icon={faHome} />
-              </a>
+          <a href="#" className="iconlist">
+            <FontAwesomeIcon icon={faHome} />
+          </a>
 
-              <a href="#products" class="iconlist">
-                <FontAwesomeIcon icon={faMapMarkerAlt} />
-              </a>
+          <a href="#products" className="iconlist">
+            <FontAwesomeIcon icon={faMapMarkerAlt} />
+          </a>
 
-              <a href="#contact" class="iconlist">
-                <FontAwesomeIcon icon={faBars} />
-              </a>
-
+          <a href="#contact" className="iconlist">
+            <FontAwesomeIcon icon={faBars} />
+          </a>
         </div>
       )}
 
       <div className="menu">
         <img className="uamLogo" src={logo} alt="Logo UAM" />
-        <button id="menu-toggle" className="menu-toggle">☰</button>
+        <button id="menu-toggle" className="menu-toggle">
+          ☰
+        </button>
         <ul id="menu-list">
-          <li><a href="#flex">Flexbox</a></li>
-          <li><a href="#products">Products</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li>
+            <a href="#flex">Flexbox</a>
+          </li>
+          <li>
+            <a href="#products">Products</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
         <div className="avatar-container">
           <img src={avatar} className="avatar" alt="Foto de perfil" />
         </div>
       </div>
+
       <div className="content" id="flex">
-        {/* Contenido de la sección Flexbox */}
+        <Slider {...sliderSettings}>
+          <div>
+            <img src={maluma} class="slidePic" />
+          </div>
+          <div>
+          <img src={maluma1} class="slidePic"/>
+          </div>
+          <div>
+          <img src={eladio} class="slidePic"/>
+          </div>
+        </Slider>
       </div>
 
       <div className="content" id="products">
-        {/* Contenido de la sección Products */}
+      <Slider {...sliderSettings}>
+          <div>
+            <img src={maluma1} class="slidePic" />
+          </div>
+          <div>
+          <img src={maluma} class="slidePic"/>
+          </div>
+          <div>
+          <img src={eladio} class="slidePic"/>
+          </div>
+        </Slider>
       </div>
 
       <div className="content" id="contact">
-        {/* Contenido de la sección Contact */}
+      <Slider {...sliderSettings}>
+          <div>
+            <img src={eladio} class="slidePic" />
+          </div>
+          <div>
+          <img src={maluma1} class="slidePic"/>
+          </div>
+          <div>
+          <img src={maluma} class="slidePic"/>
+          </div>
+        </Slider>
       </div>
 
       <div className="footer"></div>
